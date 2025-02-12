135b49a1533e5d8407ff1ccc1a9712f75361c031
Yoni190 <yfisuhu@gmail.com>
2025-02-12 17:06:37 +0300

    Create two functions to store data and one to handle the logic

---
+++ script.js
@@ -0,0 +1,22 @@
+function GameBoard(){
+    const board = [];
+    const rows = 3;
+    const columns = 3;
+
+    for(let i=0; i<rows; i++){
+        board[i] = [];
+        for(let j=0; j<columns; j++){
+            board[i].push(Square());
+        }
+    }
+    
+}
+
+
+function Square(){
+    
+}
+
+function GameController(){
+
+}
